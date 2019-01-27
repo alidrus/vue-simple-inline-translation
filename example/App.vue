497ba329f563c08c190cff8fc77ecdb886931afc
Abu Bakar Al-Idrus <alidrus@gmail.com>
2019-01-28 07:33:04 +0800

    Use a more sophisticated example

--- example/App.vue
+++ example/App.vue
@@ -1,26 +1,49 @@
 <template>
     <div id="app">
-        <translate enclosing-tag="p" language="en"
+        <select v-model='current'>
+            <option v-for="(value, key, index) in languages" v-bind:value="key">{{ value }}</option>
+        </select>
+
+        <translate enclosing-tag="h3" language="ar"
+            v-bind:current-language="current"><b>صباح</b> الخير</translate>
+
+        <translate enclosing-tag="h3" language="en"
             v-bind:current-language="current">Good <b>Morning</b></translate>
-        <translate enclosing-tag="p" language="ms"
+
+        <translate enclosing-tag="h3" language="ms"
             v-bind:current-language="current">Selamat <b>Pagi</b></translate>
+
+        <translate enclosing-tag="h3" language="sv"
+            v-bind:current-language="current">God <b>Morgon</b></translate>
+
+        <p>Hello my language is {{ languages[current] }} and the way I say welcome is:
+        <translate v-bind:current-language="current" language="en">Welcome</translate>
+        <translate v-bind:current-language="current" language="ms">Selamat Datang</translate>
+        <translate v-bind:current-language="current" language="sv">Välkommen</translate>
+        <translate v-bind:current-language="current" language="ar">اهلا و سهلا</translate>
+        </p>
     </div>
 </template>
 
 <script>
-/* eslint quote-props: ["error", "as-needed", {"unnecessary": false}] */
 import VueSimpleInlineTranslation from '@/components/VueSimpleInlineTranslation.vue';
 
 export default {
     name: 'app',
     components: {
-        'translate': VueSimpleInlineTranslation
+        translate: VueSimpleInlineTranslation
     },
     data() {
         return {
+            languages: {
+                ar: 'العربية',
+                en: 'English',
+                ms: 'Bahasa Melayu',
+                sv: 'Svenska'
+            },
             current: 'ms'
         };
-    } // eslint-disable-next-line
+    }
 }
 </script>
 
