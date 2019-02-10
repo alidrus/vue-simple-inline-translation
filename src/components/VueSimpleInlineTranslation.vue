4eb4745512bbbc2e269e33bfb0fdbaf43c7a37a6
Abu Bakar Al-Idrus <alidrus@gmail.com>
2019-02-10 08:40:10 +0800

    Added props: force-tag, tag-class, tag-style and tag-attributes

--- src/components/VueSimpleInlineTranslation.vue
+++ src/components/VueSimpleInlineTranslation.vue
@@ -5,14 +5,74 @@ export default {
             type: String,
             default: 'span'
         },
+        forceTag: {
+            type: Boolean,
+            default: false
+        },
+        tagClass: {
+            type: Object,
+            default() {
+                return {};
+            },
+            validator(obj) {
+                // empty object is valid
+                if (Object.keys(obj).length === 0) {
+                    return true;
+                }
+                // if non-empty
+                return Object.keys(obj).every(key => typeof obj[key] === 'boolean');
+            }
+        },
+        tagStyle: {
+            type: Object,
+            default() {
+                return {};
+            },
+            validator(obj) {
+                // empty object is valid
+                if (Object.keys(obj).length === 0) {
+                    return true;
+                }
+                // if non-empty
+                return Object.keys(obj).every(key => typeof obj[key] === 'string');
+            }
+        },
+        tagAttributes: {
+            type: Object,
+            default() {
+                return {};
+            },
+            validator(obj) {
+                // empty object is valid
+                if (Object.keys(obj).length === 0) {
+                    return true;
+                }
+                // if non-empty
+                return Object.keys(obj).every(key => typeof obj[key] === 'string');
+            }
+        },
         language: String,
         currentLanguage: String
     },
     render(h) {
         if (this.language === this.currentLanguage) {
-            return this.$slots.default.length === 1
+            const dataObject = {};
+            if (Object.keys(this.tagClass).length > 0) {
+                dataObject.class = this.tagClass;
+            }
+            if (Object.keys(this.tagStyle).length > 0) {
+                dataObject.style = this.tagStyle;
+            }
+            if (Object.keys(this.tagAttributes).length > 0) {
+                dataObject.attrs = this.tagAttributes;
+            }
+            console.dir({
+                slots: this.$slots.default,
+                tag: this.enclosingTag
+            });
+            return this.$slots.default.length === 1 && this.forceTag === false
                 ? this.$slots.default[0]
-                : h(this.enclosingTag, this.$slots.default);
+                : h(this.enclosingTag, dataObject, this.$slots.default);
         }
         return this._v('');
     }
